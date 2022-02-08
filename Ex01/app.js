5bd7f2c50215c38c864393fdab51aecc11cc17f9
ryan luca <58185393+ryan1235@users.noreply.github.com>
2022-02-08 04:08:03 -0300

    :bellhop_bell: novo exercícios!
    exercício 1 (registro)

---
+++ Ex01/app.js
@@ -0,0 +1,14 @@
+const nome = document.getElementById('nome')
+const idade = document.getElementById('idade')
+const linguagem =document.getElementById('linguagem')
+const res = document.getElementById('res')
+function register(){
+    if(nome.value == 0 || idade.value == 0 || linguagem.value == 0){
+        res.innerHTML = '<h2>Erro! Preencha todas os valores a cima.</h2>'
+    }else{
+        res.innerHTML = ''
+        res.innerHTML +=`<p>Olá <strong>${nome.value}</strong>!</p>`
+        res.innerHTML +=`<p>Você tem <strong>${idade.value}</strong></p>`
+        res.innerHTML +=`<p>E quer estudar <strong>${linguagem.value}</strong></p>`
+    }
+}
